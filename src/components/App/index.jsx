bc06dd1529f48299956d634851f587e72b386145
Ray <ray-227@yandex.com>
2021-04-08 15:25:40 +0500

    Подготовительные работы закончены.

---
+++ src/components/App/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.scss';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <h1>TEST</h1>
+    )
+  }
+}
---
+++ src/js/index.jsx
@@ -0,0 +1,8 @@
+import '@/styles/index.scss';
+
+import React from 'react';
+import {render} from 'react-dom';
+
+import App from '@/components/App/index.jsx';
+
+render(<App />, document.getElementById('app'));
